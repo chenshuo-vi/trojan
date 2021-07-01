979559bca4e67a855162985cd022f52d1aa94ee5
chenshuo-dr <68913746+chenshuo-dr@users.noreply.github.com>
2021-07-01 17:27:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -135,6 +135,7 @@ setupCron() {
             fi
             LOCAL_TIME2=$[1+$LOCAL_TIME]
             crontab -l 2>/dev/null|sed '/acme.sh/d' > crontab.txt
+            crontab -l 2>/dev/null|sed '/reboot/d' > crontab.txt
             echo "0 ${LOCAL_TIME}"' * * * systemctl stop trojan-web; "/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" > /dev/null; systemctl start trojan-web' >> crontab.txt
             echo "0 ${LOCAL_TIME2} * * * /sbin/reboot" >> crontab.txt
             crontab crontab.txt
